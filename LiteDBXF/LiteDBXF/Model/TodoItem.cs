c0a03716d22831bc4cf9f34cd3103b8a1fa5c086
Damien <damientohin@gmail.com>
2018-06-05 19:58:07 +0100

    I added the viewmodel and view components

---
+++ LiteDBXF/LiteDBXF/Model/TodoItem.cs
@@ -0,0 +1,19 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LiteDBXF.Model
+{
+    public class Todo : ReactiveObject
+    {
+        public string Title { get; set; }
+        bool _isDone;
+        public bool IsDone
+        {
+            get => _isDone;
+            set => this.RaiseAndSetIfChanged(ref _isDone, value);
+        }
+        public bool IsEnabled => !IsDone;
+    }
+}
---
+++ LiteDBXF/LiteDBXF/ViewModel/TodoViewModel.cs
@@ -0,0 +1,67 @@
+﻿using LiteDBXF.Model;
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Reactive.Linq;
+using System.Text;
+
+namespace LiteDBXF.ViewModel
+{
+    public class TodoViewModel : ReactiveObject
+    {
+        /// <summary>
+        /// Reactive List https://reactiveui.net/docs/handbook/collections/reactive-list
+        /// </summary>
+        ReactiveList<Todo> _todos;
+        public ReactiveList<Todo> Todos
+        {
+            get => _todos;
+            set => this.RaiseAndSetIfChanged(ref _todos, value);
+        }
+        private Todo _selectedTodo;
+        public Todo SelectedTodo
+        {
+            get => _selectedTodo;
+            set => this.RaiseAndSetIfChanged(ref _selectedTodo, value);
+        }
+        
+        private string _todoTitl;
+        public string TodoTitle
+        {
+            get { return _todoTitl; }
+            set { this.RaiseAndSetIfChanged(ref _todoTitl, value); }
+        }
+
+        public ReactiveCommand AddCommand { get; private set; }
+
+        public TodoViewModel()
+        {
+
+            AddCommand = ReactiveCommand.Create(() =>
+            {
+                Todos.Add(new Todo() { Title = TodoTitle });
+                TodoTitle = string.Empty;
+
+            }, this.WhenAnyValue(x => x.TodoTitle,
+                title =>
+                !String.IsNullOrEmpty(title)));
+
+            //Dont forget to set ChangeTrackingEnabled to true.
+            Todos = new ReactiveList<Todo>() { ChangeTrackingEnabled = true };
+
+            ///Lets detect when ever a todo Item is marked as done 
+            ///IF it is, it is sent to the bottom of the list
+            ///Else nothing happens
+            Todos.ItemChanged.Where(x => x.PropertyName == "IsDone" && x.Sender.IsDone)
+                .Select(x => x.Sender)
+                .Subscribe(x =>
+                {
+                    if (x.IsDone)
+                    {
+                        Todos.Remove(x);
+                        Todos.Add(x);
+                    }
+                });
+        }
+    }
+}
